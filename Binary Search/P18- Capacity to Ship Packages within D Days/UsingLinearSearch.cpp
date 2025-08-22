f58e8c5ca0a813058e4b35e8a57a0de2d55041a0
Anuj Modani <157504634+animus08@users.noreply.github.com>
2025-08-22 20:38:09 +0530

    Create UsingLinearSearch.cpp

---
+++ Binary Search/P18- Capacity to Ship Packages within D Days/UsingLinearSearch.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findDays(vector<int> &a, int n, int cap){
+    int days = 1, load = 0;
+    for(int i=0; i<n; i++){
+        if(load + a[i] > cap){
+            days = days + 1;
+            load = a[i];
+        }
+        else{
+            load = load + a[i];
+        }
+    }
+    return days;
+}
+
+int leaseWeightCapacity(vector<int> &a, int n, int days){
+    int maxWeight = *max_element(a.begin(), a.end()); // Used STL function to find max element
+    int sumWeight = accumulate(a.begin(), a.end(), 0); // Used STL function to find sum of elements
+
+    for(int i = maxWeight; i <= sumWeight; i++){
+        int reqDays = findDays(a, n, i);
+        if(reqDays <= days){
+            return i; // Return the minimum capacity that can ship within D days
+        }
+    }
+
+}
+
+int main(){
+    int n;
+    cout << "Enter number of packages: ";
+    cin >> n;
+    cout << endl;
+    
+    vector<int> a(n);
+    cout << "Enter weights of packages: ";
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    cout << endl;
+
+    int days;
+    cout << "Enter number of days: ";
+    cin >> days;
+    cout << endl;
+
+    int result = leaseWeightCapacity(a, n, days);
+    cout << "Minimum capacity to ship packages within " << days << " days is: " << result << endl;
+    return 0;
+}
