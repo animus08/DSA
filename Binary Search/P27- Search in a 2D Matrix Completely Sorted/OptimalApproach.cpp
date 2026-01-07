0467aaf20f90c80cba14290ddb1f0a215f31a5a7
Anuj Modani <157504634+animus08@users.noreply.github.com>
2026-01-07 20:25:42 +0530

    Create OptimalApproach.cpp

---
+++ Binary Search/P27- Search in a 2D Matrix Completely Sorted/OptimalApproach.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>> &mat, int n, int m, int target)
+{
+    int low = 0;
+    int high = (n * m) - 1; // Treat the 2D matrix as a 1D array
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int row = mid / m; // Calculate the row index
+        int col = mid % m; // Calculate the column index
+        if (mat[row][col] == target)
+        {
+            return true; // Return true if target is found
+        }
+        else if (mat[row][col] < target)
+            low = mid + 1; // Move to the right half
+        else
+            high = mid - 1; // Move to the left half
+    }
+    return false; // Return false if target is not found
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of rows in the matrix: ";
+    cin >> n;
+
+    int m;
+    cout << "Enter the number of columns in the matrix: ";
+    cin >> m;
+
+    vector<vector<int>> mat(n, vector<int>(m));
+    cout << "Enter the elements of the matrix: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> mat[i][j];
+        }
+    }
+
+    cout << "Matrix entered:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int target;
+    cout << "Enter the target element to search: ";
+    cin >> target;
+
+    if (searchMatrix(mat, n, m, target) == true)
+    {
+        cout << "Element found in the matrix." << endl;
+    }
+    else
+    {
+        cout << "Element not found in the matrix." << endl;
+    }
+}
