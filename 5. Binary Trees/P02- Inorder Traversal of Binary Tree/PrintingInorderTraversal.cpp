92f890a4f4ae5db9804cd8cfa4f363f46df59eec
Anuj Modani <157504634+animus08@users.noreply.github.com>
2026-01-11 23:54:44 +0530

    Create PrintingInorderTraversal.cpp

---
+++ 5. Binary Trees/P02- Inorder Traversal of Binary Tree/PrintingInorderTraversal.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void inorder(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main()
+{
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    cout << "Inorder Traversal: ";
+    inorder(root);
+    cout << endl;
+}
