611631943afb796b14dfaac887aadbbfde6f32e2
Anuj Modani <157504634+animus08@users.noreply.github.com>
2026-02-07 20:53:03 +0530

    Create PrintingLeftView.cpp

---
+++ 5. Binary Trees/P21- Left View of Binary Tree/PrintingLeftView.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void recursion(TreeNode *root, int level, vector<int> &ans)
+{
+    if (root == NULL)
+        return;
+
+    if (ans.size() == level)
+    {
+        ans.push_back(root->data);
+    }
+
+    recursion(root->left, level + 1, ans);
+    recursion(root->right, level + 1, ans);
+}
+
+vector<int> leftView(TreeNode *root)
+{
+    vector<int> ans;
+    recursion(root, 0, ans);
+    return ans;
+}
+
+int main()
+{
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    root->right->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->left->right = new TreeNode(7);
+
+    vector<int> ans = leftView(root);
+    cout << "The left view of the binary tree is: ";
+    for (int i : ans)
+    {
+        cout << i << " ";
+    }
+}
